94259bf208dd25d8e79d11e82c28b62cad91cdf7
Antonio Pérez García <Antonio Prez Garca>
2017-05-03 22:49:29 +0200

    Añadir jquery

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <h1></h1>
+    <h1>texto de jquery</h1>
     <h2>Essential Links</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
@@ -20,12 +20,16 @@
 </template>
 
 <script>
+
 export default {
   name: 'app',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  mounted () {
+    console.log($("h1").text());
   }
 }
 </script>
